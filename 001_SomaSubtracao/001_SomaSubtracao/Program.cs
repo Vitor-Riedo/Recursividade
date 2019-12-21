87b257dc7aa12ab5c7e8d3a548957db3f50096cf
Vitor Riedo <vitorriedo@hotmail.com>
2019-12-21 19:55:59 -0200

    Soma: For, Recursiva com e sem Index, somando na ida e volta.

---
+++ 001_SomaSubtracao/001_SomaSubtracao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SomaSubtracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdNumero = 5;
+
+            //Gera uma lista de números.
+            List<int> lista = Gerador.geraListaNumero(qtdNumero);
+
+            //Faz uma cópia da lista pois tem método que remove os elementos da mesma.
+            List<int> listaAux = new List<int>(lista.ToArray()); 
+
+            Gerador.exibeListaNumero(qtdNumero, lista);
+
+            Console.WriteLine("Soma não Recursiva: " + Soma.somaNormal(lista));
+
+            Console.WriteLine("\nSoma na Volta Recursiva Com Index: " + Soma.somaVoltaRecursivaComIndex(0,lista));
+
+            Console.WriteLine("\nSoma na Volta Recursiva Sem Index: " + Soma.somaVoltaRecursivaSemIndex(listaAux));
+            listaAux = new List<int>(lista.ToArray()); 
+
+            Console.WriteLine("\nSoma na Ida Recursiva Com Index: " + Soma.somaIdaRecursivaComIndex(0,0,lista));
+
+            Console.WriteLine("\nSoma na Ida Recursiva Sem Index: " + Soma.somaIdaRecursivaSemIndex(0, lista));
+
+            Console.WriteLine("\n\nPrecione alguma tecla para continuar!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 001_SomaSubtracao/001_SomaSubtracao/Soma.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SomaSubtracao
+{
+    class Soma
+    {
+
+        public static int somaNormal(List<int> lista)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                soma += lista[i];
+            }
+            
+            return soma;
+        }
+
+        public static int somaVoltaRecursivaComIndex(int pos,List<int> lista)
+        {
+            if (lista.Count-1 == pos)
+                return lista[pos];
+
+            return lista[pos] + somaVoltaRecursivaComIndex(++pos,lista); //++ antes para incrementar antes de mandar
+        }
+
+        public static int somaVoltaRecursivaSemIndex(List<int> lista)
+        {
+            if (lista.Count == 1)
+                return lista[0];
+
+            int valor = lista[0];   //Salva o valor pra volta somar.
+            lista.RemoveAt(0);      //Remove o primeiro valor para passar os demais.
+
+            return valor + somaVoltaRecursivaSemIndex(lista);
+        }
+
+        public static int somaIdaRecursivaComIndex(int soma, int pos,List<int> lista)
+        {
+            soma += lista[pos++];   //Já incrementa para poder verificar no if, o problema é se mandar uma lista vazia. Dá pra fazer com o if antes.
+
+            if (lista.Count == pos) //Não tem -1 no Count pois o pos já foi incrementado.
+                return soma;
+
+            return somaIdaRecursivaComIndex(soma, pos, lista);
+        }
+
+        public static int somaIdaRecursivaSemIndex(int soma,List<int> lista)
+        {
+            soma += lista[0];   //Soma o valor antes de mandar.
+
+            if (lista.Count == 1) //Se a coma for depois do if é preciso fazer lista.Count == 0 
+                return soma;
+
+            //soma += lista[0];   //Soma o valor antes de mandar.
+            lista.RemoveAt(0);    //Remove o primeiro valor para passar os demais.
+
+            return somaIdaRecursivaSemIndex(soma,lista);
+        }
+    }
+}
