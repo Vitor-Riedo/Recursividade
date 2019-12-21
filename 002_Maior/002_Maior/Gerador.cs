3ac9570af5189543b5c6a4b94cd69dcb9b040626
Vitor Riedo <vitorriedo@hotmail.com>
2019-12-21 20:32:36 -0200

    Criação projeto 002_Soma

---
+++ 002_Maior/002_Maior/Gerador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maior
+{
+    class Gerador
+    {
+
+        public static List<int> geraListaNumero(int qtd)
+        {
+            List<int> lista = new List<int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < qtd; i++)
+                lista.Add(random.Next(1,10));         
+
+            return lista;
+        }
+
+        public static void exibeListaNumero(int qtd, List<int> lista)
+        {
+            Console.WriteLine("Números Gerados\n");
+            for (int i = 0; i < qtd; i++)
+                Console.WriteLine(lista[i]);
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ 002_Maior/002_Maior/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdNumero = 5;
+
+            //Gera uma lista de números.
+            List<int> lista = Gerador.geraListaNumero(qtdNumero);
+
+            //Faz uma cópia da lista pois tem método que remove os elementos da mesma.
+            List<int> listaAux = new List<int>(lista.ToArray());
+
+            Console.WriteLine("Programa para retornar o maior número de uma lista de números de forma Recursiva e Iterativa - Vitor Riedo\n");
+
+            Gerador.exibeListaNumero(qtdNumero, lista);
+
+            Console.WriteLine("Maior Iterativo: " + Maior.maiorIterativo(lista));
+
+            /*Console.WriteLine("\nSoma na Volta Recursiva Com Index: " + Soma.somaVoltaRecursivaComIndex(0, lista));
+
+            Console.WriteLine("\nSoma na Volta Recursiva Sem Index: " + Soma.somaVoltaRecursivaSemIndex(listaAux));
+            listaAux = new List<int>(lista.ToArray());
+
+            Console.WriteLine("\nSoma na Ida Recursiva Com Index: " + Soma.somaIdaRecursivaComIndex(0, 0, lista));
+
+            Console.WriteLine("\nSoma na Ida Recursiva Sem Index: " + Soma.somaIdaRecursivaSemIndex(0, lista));
+            */
+            Console.WriteLine("\n\nPrecione alguma tecla para continuar!!!");
+            Console.ReadKey();
+        }
+    }
+}
